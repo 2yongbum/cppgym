b1fc087aa70dd43a6cce68a8fc94ae44398fd4e7
Yongbum Lee <2yongbum@gmail.com>
2019-03-04 08:37:11 +0900

    add c struct

--- struct class/readme.md
+++ struct class/readme.md
@@ -1,3 +1,4 @@
+1. struct in c vs. c++
 1. struct vs. class
 1. extend struct by class
 1. construct in struct
---
+++ struct class/struct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct AAA
+{
+    int a;
+    int b;
+};
+
+int main()
+{
+    AAA aaa;
+    aaa.a = 10;
+    return 0;
+}
